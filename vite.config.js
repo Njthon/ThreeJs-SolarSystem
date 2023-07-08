43716d146280647ad5e493931457a3639bf8ab74
UndefinedGrunt <hassler.leonard@live.com>
2023-07-08 09:41:51 +0200

    14th commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/ThreeJs-SolarSystem/'
+    // Base URL for production builds
+    base: '/ThreeJs-SolarSystem/',
+
+    // Development server configuration
+    server: {
+        port: 3000, // Specify the port you want to use for the development server
+    },
+
+    // Build configuration for production
+    build: {
+        outDir: 'dist', // Specify the output directory for the production build
+        assetsDir: '', // Specify the assets directory relative to the output directory
+        sourcemap: false, // Set this to true if you want to generate sourcemaps for debugging
+    },
 });
